9fed62ceb95d0523edbb08a567b367203a1aa89b
Root <root@queryable.com>
2022-12-01 16:34:25 +0100

    decrease number of parallel threads to fix syncing issue

--- crates/sui-core/src/node_sync/node_state.rs
+++ crates/sui-core/src/node_sync/node_state.rs
@@ -40,11 +40,11 @@ use tracing::{debug, error, instrument, trace, warn};
 
 const NODE_SYNC_QUEUE_LEN: usize = 500;
 
-// Process up to 20 digests concurrently.
-const MAX_NODE_SYNC_CONCURRENCY: usize = 20;
+// Process up to 10 digests concurrently.
+const MAX_NODE_SYNC_CONCURRENCY: usize = 10;
 
-// All tasks die after 60 seconds if they haven't finished.
-const MAX_NODE_TASK_LIFETIME: Duration = Duration::from_secs(60);
+// All tasks die after 180 seconds if they haven't finished.
+const MAX_NODE_TASK_LIFETIME: Duration = Duration::from_secs(180);
 
 // How long to wait for parents to be processed organically before fetching/executing them
 // directly.
